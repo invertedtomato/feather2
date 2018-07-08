f3e2747c4a5d206db33199b5993993a66a63e2c1
Ben Thompson <ben@invertedtomato.com>
2018-07-08 20:32:35 +1000

    Fixed several unit tests.

--- NetLibraryTests/FeatherUdpPeerTests.cs
+++ NetLibraryTests/FeatherUdpPeerTests.cs
@@ -6,6 +6,7 @@ using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
+using System.Linq;
 
 namespace NetLibraryTests {
     public class FeatherUdpPeerTests {
@@ -25,7 +26,7 @@ namespace NetLibraryTests {
             var len = socket.Receive(buf);
 
             Assert.Equal(3, len);
-            Assert.Equal(new byte[] { 1, 2, 3 }, buf);
+            Assert.Equal(new byte[] { 1, 2, 3 }, buf.Take(3));
         }
 
         [Fact]
@@ -44,12 +45,30 @@ namespace NetLibraryTests {
             var len = socket.Receive(buf);
 
             Assert.Equal(3, len);
-            Assert.Equal(new byte[] { 1, 2, 3 }, buf);
+            Assert.Equal(new byte[] { 1, 2, 3 }, buf.Take(3));
         }
 
         [Fact]
         public void ReceiveMessage() {
             throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async void Dispose() {
+            var peer = new FeatherUdpPeer<BinaryMessage>();
+            Assert.False(peer.IsDisposed);
+            peer.Bind(12347);
+            Assert.False(peer.IsDisposed);
+            peer.Dispose();
+            Assert.True(peer.IsDisposed);
+
+            Assert.Throws<ObjectDisposedException>(() => {
+                peer.SendToSync(new IPEndPoint(IPAddress.Loopback, 12346), new BinaryMessage(new byte[] { 1, 2, 3 }));
+            });
+
+            await Assert.ThrowsAsync<ObjectDisposedException>(async () => {
+                await peer.SendTo(new IPEndPoint(IPAddress.Loopback, 12346), new BinaryMessage(new byte[] { 1, 2, 3 }));
+            });
         }
     }
 }
