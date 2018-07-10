6deee8d2f31b56550350a842db61a05d68c9815f
Ben Thompson <ben@invertedtomato.com>
2018-07-10 19:13:00 +1000

    Tidying.

--- NetLibrary/Net/Feather/FeatherTcpServer.cs
+++ NetLibrary/Net/Feather/FeatherTcpServer.cs
@@ -2,45 +2,46 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace InvertedTomato.Net.Feather
-{
-    class FeatherTcpServer
-    {
+namespace InvertedTomato.Net.Feather {
+    public class FeatherTcpServer {
+        private readonly Socket Underlying = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        private readonly Object Sync = new Object();
 
-        //public event Action<EndPoint> OnClientConnected;
-        //public event Action<EndPoint, DisconnectionType> OnClientDisconnected;
+        public event Action<EndPoint, TMessage> OnMessageReceived;
+        public event Action<EndPoint> OnClientConnected;
+        public event Action<EndPoint, DisconnectionType> OnClientDisconnected;
+
+        public bool IsDisposed { get; private set; }
 
         public void Listen(UInt32 port) { // Tcp, Udp
             throw new NotImplementedException();
-        }
-        public void ListenSecure(UInt32 port, X509Certificate certificate) { // TcpSsl
-
-        }
-        public void Unlisten() { // Tcp, TcpSsl, Udp
-
         }
         public void Disconnect(EndPoint address) { // Tcp-server, TcpSsl-server
             throw new NotImplementedException();
         }
 
 
-        public void Connect(EndPoint address) { // Tcp
-            throw new NotImplementedException();
-        }
-        public void ConnectSecure(EndPoint address) { // TcpSsl
-
-        }
-        public void Disconnect() { // Tcp-client, TcpSsl-client
-
-        }
 
 
-        public void SendTo(? address, TMessage msg) { // Tcp, TcpSsl, Udp
+
+        public void SendTo(EndPoint address, TMessage msg) { // Tcp, TcpSsl, Udp
             throw new NotImplementedException();
         }
 
-        public void Send(TMessage msg) { // Tcp-client, TcpSsl-client
-            throw new NotImplementedException();
+        protected virtual void Dispose(bool disposing) {
+            if (IsDisposed) {
+                return;
+            }
+            IsDisposed = true;
+
+            if (disposing) {
+                // Dispose managed state (managed objects)
+                Underlying.Dispose();
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
         }
     }
 }
--- NetLibrary/Net/Feather/FeatherUdpPeer.cs
+++ NetLibrary/Net/Feather/FeatherUdpPeer.cs
@@ -5,22 +5,103 @@ using System.Threading;
 using System.Threading.Tasks;
 using InvertedTomato.IO.Messages;
 
-// TODO: Async
 namespace InvertedTomato.Net.Feather {
     public class FeatherUdpPeer<TMessage> : IDisposable where TMessage : IImportableMessage, IExportableMessage, new() {
         private readonly Socket Underlying = new Socket(SocketType.Dgram, ProtocolType.Udp);
         private readonly Object Sync = new Object();
 
+        /// <summary>
+        /// Fired when a message is recieved. The message is provided, along with the remote endpoint that sent the message.
+        /// </summary>
         public event Action<EndPoint, TMessage> OnMessageReceived;
 
-        public Int32 ReceiveBufferSize { get { return Underlying.ReceiveBufferSize; } set { Underlying.ReceiveBufferSize = value; } }
+        /// <summary>
+        /// The maximum message size that will be accepted. If the message is less than this size it will be truncaded. Note that over the internet using IPv4 508 is the maximum safe size, and 1,212 over IPv6.
+        /// </summary>
         public Int32 ReceiveMaxMessageSize { get; set; } = 1500;
 
+        /// <summary>
+        /// Is it disposed?
+        /// </summary>
         public bool IsDisposed { get; private set; }
 
-        public FeatherUdpPeer() {
-            
+        /// <summary>
+        /// Bind to a local port on all IP addresses.
+        /// </summary>
+        /// <param name="port"></param>
+        public void Bind(Int32 port) {
+            Bind(new IPEndPoint(IPAddress.Any, port));
         }
+
+        /// <summary>
+        /// Bind to a local port on a specified IP address.
+        /// </summary>
+        /// <param name="localEndPoint"></param>
+        public void Bind(EndPoint localEndPoint) {
+            lock (Sync) {
+                // Bind underlying socket
+                Underlying.Bind(localEndPoint);
+
+                // Start receiving
+                StartReceive();
+            }
+        }
+
+        /// <summary>
+        /// Send a message to a given remote endpoint asynchronously.
+        /// </summary>
+        /// <param name="remoteEndPoint"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task SendTo(EndPoint remoteEndPoint, TMessage message) {
+            if (null == remoteEndPoint) {
+                throw new ArgumentNullException(nameof(remoteEndPoint));
+            }
+            if (null == message) {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            // Extract payload from message
+            var payload = message.Export();
+
+            // Send!
+            await Underlying.SendToAsync(payload, SocketFlags.None, remoteEndPoint);
+        }
+
+        /// <summary>
+        /// Send a message to a given remote endpoint synchronously.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="message"></param>
+        public void SendToSync(EndPoint target, TMessage message) {
+            if (null == message) {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            // Extract payload from message
+            var payload = message.Export();
+
+            // Send!
+            Underlying.SendTo(payload.Array, payload.Offset, payload.Count, SocketFlags.None, target);
+        }
+        
+        protected virtual void Dispose(bool disposing) {
+            if (IsDisposed) {
+                return;
+            }
+            IsDisposed = true;
+
+            if (disposing) {
+                // Dispose managed state (managed objects)
+                Underlying.Dispose();
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+        }
+
+
 
         private void StartReceive() {
             var args = new SocketAsyncEventArgs();
@@ -41,60 +122,6 @@ namespace InvertedTomato.Net.Feather {
             try {
                 Underlying.ReceiveMessageFromAsync(args);
             } catch (ObjectDisposedException) { };
-        }
-
-        public void Bind(Int32 port) {
-            Bind(new IPEndPoint(IPAddress.Any, port));
-        }
-        public void Bind(EndPoint endPoint) {
-            lock (Sync) {
-                // Bind underlying socket
-                Underlying.Bind(endPoint);
-
-                // Start receiving
-                StartReceive();
-            }
-        }
-
-        public async Task SendTo(EndPoint target, TMessage message) {
-            if (null == message) {
-                throw new ArgumentNullException(nameof(message));
-            }
-
-            // Extract payload from message
-            var payload = message.Export();
-
-            // Send!
-            await Underlying.SendToAsync(payload, SocketFlags.None, target);
-        }
-
-        public void SendToSync(EndPoint target, TMessage message) {
-            if (null == message) {
-                throw new ArgumentNullException(nameof(message));
-            }
-
-            // Extract payload from message
-            var payload = message.Export();
-
-            // Send!
-            Underlying.SendTo(payload.Array, payload.Offset, payload.Count, SocketFlags.None, target);
-        }
-
-
-        protected virtual void Dispose(bool disposing) {
-            if (IsDisposed) {
-                return;
-            }
-            IsDisposed = true;
-
-            if (disposing) {
-                // Dispose managed state (managed objects)
-                Underlying.Dispose();
-            }
-        }
-
-        public void Dispose() {
-            Dispose(true);
         }
     }
 }
